fba5adde5ef0ffabb5b55e3f008c40714e71018b
Indra F <indrafebriasta@gmail.com>
2018-09-23 15:49:01 +0700

    testing

---
+++ src/pbo6k/pkg10116588/latihan1/biodata/PBO6k10116588Latihan1Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116588.latihan1.biodata;
+
+/**
+ *
+ * @author Indra F
+ * Nama                     : Indra Febriastanto
+ * NIM                      : 10116588
+ * KELAS                    : PBO6K
+ * Deskripsi program        : Program ini berisi untuk menampilkan
+ * Jenis - jenis tipe data  : string,bilangan bulat
+ */
+public class PBO6k10116588Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("---*_*_*_*_*_*_" + "BIODATA" + "*_*_*_*_*_*---" + "\n" + "-----------------------------------");
+        System.out.println("NAMA\t\t : Indra Febriastanto");
+        System.out.println("ALAMAT\t\t : jl.gagak no 128");
+        System.out.println("TEMPAT LAHIR\t : Sragen");
+        System.out.println("TANGGAL LAHIR \t : 20 Februari 1998");
+        System.out.println("------------------------------------");
+        System.out.println("HOBI\t\t : Futsal");
+        System.out.println("MAKES\t\t : Bakso");
+        System.out.println("MIKES\t\t : Jus Alpukat");
+    }
+    
+     
+}
